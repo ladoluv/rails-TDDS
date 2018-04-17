48745315f3828f4a61bbfc49d6a37fb8a207111d
GIRISH <bobbyhimvihar@gmail.com>
2018-04-17 17:01:42 -0400

    some changes in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :tax_searches
+  resources :tax_searches, path: "tax-searches"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
